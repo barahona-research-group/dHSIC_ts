2fd20e516a6cb7e97f13a4789c3c3c9ffdc34bd5
Tim Liu <tim1997liu@hotmail.com>
2022-03-13 18:19:22 +0800

    simplified data code

--- data.py
+++ data.py
@@ -22,38 +22,6 @@ def width(Z):
     return width_Z
 
 
-def make_K_list(X_list, n_samples, n_nodes):
-    """
-    Computes the kernel matrices of the variables in X_array, where each column represents one variable.
-    Returns a list of the kernel matrices of each variable.
-    """
-    k_list = list(
-        pairwise_kernels(X_list[i], metric='rbf', gamma=0.5 / (width(X_list[i]) ** 2)) for i in range(n_nodes))
-    return k_list
-
-
-# # data preparation
-# groups_prep_g = {}
-# groups_prep_g_K = {}
-#
-# for group in groups:
-#     print(group)
-#
-#     groups_prep_g[group] = np.empty(17, dtype=object)
-#     groups_prep_g_K[group] = np.empty(17, dtype=object)
-#
-#     for g, goal in enumerate(goals):
-#         g_list = []
-#         for country in groups[group].dropna():
-#             g_list.append(np.asarray(goals_values_i[country][g]))
-#
-#         g_array = np.asarray(g_list)
-#         groups_prep_g[group][g] = g_array
-#
-#         K_matrix = pairwise_kernels(g_array, metric='rbf', gamma=0.5 / (width(g_array) ** 2))
-#         groups_prep_g_K[group][g] = K_matrix
-
-
 def compute_kernel(data, nodes):
     # data preparation
 
